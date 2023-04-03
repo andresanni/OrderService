1c87b4d49a9c465478aef60293b23f062068cb92
Andres Anni <andres.anni1985@gmail.com>
2023-04-03 10:08:47 -0300

    Servicio para guardar ordenes completo

---
+++ src/main/java/com/andresanni/orderservice/service/OrderService.java
@@ -0,0 +1,43 @@
+package com.andresanni.orderservice.service;
+
+import com.andresanni.orderservice.dto.OrderLineItemdsDto;
+import com.andresanni.orderservice.dto.OrderRequest;
+import com.andresanni.orderservice.model.Order;
+import com.andresanni.orderservice.model.OrderLineItems;
+import com.andresanni.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+
+    public void placeOrder(OrderRequest orderRequest){
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItems> orderLineItems =
+                orderRequest.getOrderLineItemsDtoList().stream()
+                .map(this::mapToDto)
+                .collect(Collectors.toList());
+
+        order.setOrderLineItemsList(orderLineItems);
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemdsDto orderLineDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineDto.getPrice());
+        orderLineItems.setQuantity(orderLineDto.getQuantity());
+        orderLineItems.setSkuCode(orderLineDto.getSkuCode());
+        return orderLineItems;
+    }
+
+}
